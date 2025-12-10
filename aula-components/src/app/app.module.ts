db8da5e49127ba1433d16b5e61e060aa7c7fbab9
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-10 10:02:49 -0300

    aula 28 - pt1

--- aula-components/src/app/app.module.ts
+++ aula-components/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CardRoxoComponent
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
   ],
   imports: [
     BrowserModule
