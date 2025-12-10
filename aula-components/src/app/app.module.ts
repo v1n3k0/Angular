af46b6277567eba7ac6ada8b0cf2f89ad75b95be
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-10 16:23:53 -0300

    aula 30

--- aula-components/src/app/app.module.ts
+++ aula-components/src/app/app.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
-import { CardButtonComponent } from './card-button/card-button.component';
-import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
+import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CardComponent,
-    CardRoxoComponent,
-    CardButtonComponent,
-    CardRoxoButtonComponent
+    AppComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ aula-components/src/app/cards/cards.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CardComponent,
+    CardRoxoComponent
+  ]
+})
+export class CardsModule { }
