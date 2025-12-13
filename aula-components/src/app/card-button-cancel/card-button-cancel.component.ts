a500f9d5d0ec2d55893ff5cda3b616fd14ed0285
Vinicius <v1n1c1u5_1@hotmail.com>
2025-12-13 16:00:56 -0300

    aula 35

--- aula-components/src/app/card-button-cancel/card-button-cancel.component.ts
+++ aula-components/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,20 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  // styleUrls: ['./card-button-cancel.component.scss'],
-  styles: [
-    ` .card-button {
-        font-size: 16px;
-        text-align: center;
-        margin-top: 10px;
-        border-radius: 25px;
-        background-color: red;
-        cursor: pointer;
-        font-weight: bold;
-        padding: 10px;
-      }
-    `
-  ]
+  styleUrls: ['./card-button-cancel.component.scss'],
 })
 export class CardButtonCancelComponent {
 
