3f436645be4406296cbdf818be189c675c34a3ec
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-14 20:33:06 -0300

    aula 44

--- aula-components/src/app/card/card.component.ts
+++ aula-components/src/app/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CardComponent {
  plano = {
