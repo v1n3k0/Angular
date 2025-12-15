babfb698400b6b1e0efb913344f0e09c55884fa7
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-15 19:06:13 -0300

    aula 57

--- aula-binding/src/app/app.component.spec.ts
+++ aula-binding/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'style-binding'`, () => {
+  it(`should have as title 'class-binding'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('style-binding');
+    expect(app.title).toEqual('class-binding');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('style-binding app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('class-binding app is running!');
   });
 });
--- aula-binding/src/app/app.component.ts
+++ aula-binding/src/app/app.component.ts
@@ -6,24 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  widthButton1 = '110px';
-  widthButton2 = 130;
-  stylesObj = { width: '160px', backgroundColor: 'grey' };
-
-  updateStyleObj() {
-    console.log('updateStyleObj');
-
-    this.stylesObj.width = '170px';
-    this.stylesObj.backgroundColor = 'lightblue';
-  }
-
-  updateStyleObjCorrect() {
-    console.log('updateStyleObjCorrect');
-
-    this.stylesObj = { width: '170px', backgroundColor: 'lightblue' };
-  }
-
-  updateWidth() {
-    this.widthButton2 = 200;
-  }
+  isRedButton = true;
+  isGreen = true;
 }
