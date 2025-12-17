b9a098abdbc813834031fd80191249bc95a55dc9
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-17 19:42:25 -0300

    aula 72 pt2

--- aula-ngfor/src/app/app.module.ts
+++ aula-ngfor/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule
