b2d8361dcb1e71afc948d329d09f60befd9a75f3
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-16 21:18:52 -0300

    aula 64 pt2

--- aula-cards/src/app/app.component.ts
+++ aula-cards/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  buttonClicked() {
+    console.log('Button was clicked!');
+  }
 }
--- aula-cards/src/app/components/button/button.component.ts
+++ aula-cards/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,9 +6,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
-  @Input({ required: true, alias: 'text' }) 
-    buttonText: string = '';
+  @Input({ required: true, alias: 'text' })
+  buttonText: string = '';
 
-  @Input({ required: true, alias: 'style' }) 
-    buttonStyle: 'white' | 'purple' = 'white';
+  @Input({ required: true, alias: 'style' })
+  buttonStyle: 'white' | 'purple' = 'white';
+
+  @Input({ alias: 'disabled' })
+  isDisabled: boolean = false;
+
+  @Output('clicked') buttonClicked = new EventEmitter<void>();
+
+  onButtonClicked() {
+    this.buttonClicked.emit();
+  }
 }
