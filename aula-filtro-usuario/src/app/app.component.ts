9afb70510bcbfda83d7d01ad73d10f80048ae7d9
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-20 16:02:00 -0300

    aula 85

--- aula-filtro-usuario/src/app/app.component.ts
+++ aula-filtro-usuario/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aula-filtro-usuario';
+  userSelected: IUser = UsersList[0]; 
 }
