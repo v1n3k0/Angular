b61a5e0e5cac06d0d8e2aeaa1bc0af313fd76bb9
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-21 19:07:34 -0300

    aula 92

--- aula-filtro-usuario/src/app/app.component.ts
+++ aula-filtro-usuario/src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'aula-filtro-usuario';
+export class AppComponent implements OnInit {
+    userList: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.userList = UsersList;
+    }, 1000);
+  }
 
   onUserSelected(user: IUser): void {
     this.userSelected = user;
--- aula-filtro-usuario/src/app/components/user-list/user-list.component.ts
+++ aula-filtro-usuario/src/app/components/user-list/user-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { UsersList } from 'src/app/data/users-list';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUser } from 'src/app/interfaces/user/user.interface';
 
 @Component({
@@ -9,7 +8,7 @@ import { IUser } from 'src/app/interfaces/user/user.interface';
 })
 export class UserListComponent {
   displayedColumns: string[] = ['name', 'date', 'Status'];
-  userList: IUser[] = UsersList;
+  @ Input({ required: true }) userList: IUser[] = [];
 
   @Output('userSelected') userSelectedEmitt = new EventEmitter<IUser>();
 
