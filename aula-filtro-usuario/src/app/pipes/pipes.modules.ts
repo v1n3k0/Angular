76d44c6f28445d4ae99a722718464e7bf574fb9c
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-21 17:39:40 -0300

    aula 90

--- aula-filtro-usuario/src/app/pipes/pipes.modules.ts
+++ aula-filtro-usuario/src/app/pipes/pipes.modules.ts
@@ -3,17 +3,20 @@ import { NgModule } from "@angular/core";
 import { PhonePipe } from "./phone.pipe";
 import { StatusPipe } from "./status.pipe";
 import { AddressPipe } from "./address.pipe";
+import { DashIfEmptyPipe } from "./dash-if-empty.pipe";
 
 @NgModule({
   declarations: [
     PhonePipe,
     StatusPipe,
-    AddressPipe
+    AddressPipe,
+    DashIfEmptyPipe,
   ],
   exports: [
     PhonePipe,
     StatusPipe,
-    AddressPipe
+    AddressPipe,
+    DashIfEmptyPipe,
   ],
 })
 export class PipesModule {}
