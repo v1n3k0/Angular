41dfff1ef9647c79649cb7d74f381e0acfc451eb
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-21 17:17:12 -0300

    aula 88

--- aula-filtro-usuario/src/app/pipes/pipes.modules.ts
+++ aula-filtro-usuario/src/app/pipes/pipes.modules.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 
 import { PhonePipe } from "./phone.pipe";
+import { StatusPipe } from "./status.pipe";
 import { AddressPipe } from "./address.pipe";
 
 @NgModule({
   declarations: [
     PhonePipe,
+    StatusPipe,
     AddressPipe
   ],
   exports: [
     PhonePipe,
+    StatusPipe,
     AddressPipe
   ],
 })
