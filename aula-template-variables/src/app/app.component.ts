577fb04e25d4bf4de7150983e02ea3c9f15f164a
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-23 15:06:52 -0300

    aula 106

--- aula-template-variables/src/app/app.component.ts
+++ aula-template-variables/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FilhoComponent } from './filho/filho.component';
 
 @Component({
@@ -6,11 +6,56 @@ import { FilhoComponent } from './filho/filho.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @ViewChild(FilhoComponent) filhoCompRef!: FilhoComponent;
+export class AppComponent implements AfterViewInit {
 
-  hello() {
-    this.filhoCompRef.dizerOi();
-    this.filhoCompRef.message = 'Mensagem alterada pelo Pai!';
+  buttonList = [
+    'Botão 1',
+    'Botão 2',
+    'Botão 3'
+  ]
+
+  @ViewChildren('meuButton')
+  buttonsEl!: QueryList<ElementRef<HTMLButtonElement>>;
+
+  ngAfterViewInit(): void {
+    console.log(this.buttonsEl);
+
+    this.buttonsEl.changes.subscribe( (result) => {
+      console.log('Mudou a lista de botões');
+      console.log(result);
+    });
+  }
+
+  changeColor(event: Event) {
+    console.log(event);
+
+    const btnElement = event.target as HTMLButtonElement;
+    btnElement.style.backgroundColor = 'red';
+    btnElement.style.color = 'white';
+  }
+
+  resetButton() {
+    this.buttonsEl.forEach((btn) => {
+      btn.nativeElement.style.backgroundColor = '';
+      btn.nativeElement.style.color = '';
+    });
+  }
+
+  first() {
+    // const firstBtn = this.buttonsEl.first;
+    // firstBtn.nativeElement.style.backgroundColor = 'blue';
+    // firstBtn.nativeElement.style.color = 'white';
+
+    // const firstBtn = this.buttonsEl.find( (btnEl) => btnEl.nativeElement.className === 'btn-0' );
+    // firstBtn!.nativeElement.style.backgroundColor = 'blue';
+    // firstBtn!.nativeElement.style.color = 'white';
+
+    const firstBtn = this.buttonsEl.toArray()[0];
+    firstBtn.nativeElement.style.backgroundColor = 'blue';
+    firstBtn.nativeElement.style.color = 'white';
+  }
+
+  remove() {
+    this.buttonList.shift();
   }
 }
