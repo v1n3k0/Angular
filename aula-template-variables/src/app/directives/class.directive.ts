77a7ecfa3f549641d6f97744acb4ce44b332ed34
v1n3k0 <v1n1c1u5_1@hotmail.com>
2025-12-24 12:39:08 -0300

    aula 113

--- aula-template-variables/src/app/directives/class.directive.ts
+++ aula-template-variables/src/app/directives/class.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, Host, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
-
-  constructor() { }
-
+// @HostBinding('attr.class') elementClass = 'meu-texto';
+// @HostBinding('className') elementClass = 'meu-texto meu-size';
+  @HostBinding('class') elementClass = {
+    'meu-texto': true,
+    'meu-size': true
+  };
 }
