b00bb8455ecc9eadeefcd96c0e0ae35d5d94bc92
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-01-12 09:22:55 -0300

    aula 133

--- aula-card-project/src/app/app.module.ts
+++ aula-card-project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BalanceCardComponent } from './components/balance-card/balance-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BalanceCardComponent
   ],
   imports: [
     BrowserModule
