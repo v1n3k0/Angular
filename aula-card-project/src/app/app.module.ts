dce242b0d4a8efad0345927692b727a52c73358f
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-01-12 17:41:13 -0300

    aula 140

--- aula-card-project/src/app/app.module.ts
+++ aula-card-project/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CreditCardComponent } from './components/credit-card/credit-card.compon
 import { DebitCardComponent } from './components/debit-card/debit-card.component';
 import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
 import { CardItemComponent } from './components/card-item/card-item.component';
+import { CardLineComponent } from './components/card-line/card-line.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CardItemComponent } from './components/card-item/card-item.component';
     CreditCardComponent,
     DebitCardComponent,
     ClientInfosCardComponent,
-    CardItemComponent
+    CardItemComponent,
+    CardLineComponent
   ],
   imports: [
     BrowserModule
