80723f91de038714867ce324a5914d4e0e992153
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-01-13 09:49:25 -0300

    aula 146

--- aula-projecao/src/app/app.module.ts
+++ aula-projecao/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CompTextoComponent } from './comp-texto/comp-texto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CompTextoComponent
   ],
   imports: [
     BrowserModule
---
+++ aula-projecao/src/app/comp-texto/comp-texto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-texto',
+  templateUrl: './comp-texto.component.html',
+  styleUrl: './comp-texto.component.scss'
+})
+export class CompTextoComponent {
+
+}
