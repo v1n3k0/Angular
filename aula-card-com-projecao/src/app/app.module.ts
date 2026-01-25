0416fdedfcb4a514485c51f0f31123f99e7b17e0
Vinicius <v1n1c1u5_1@hotmail.com>
2026-01-25 15:05:04 -0300

    aula 154

--- aula-card-com-projecao/src/app/app.module.ts
+++ aula-card-com-projecao/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardHeaderDirective } from './card/directives/card-header.directive';
+import { CardContentDirective } from './card/directives/card-content.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardHeaderDirective,
+    CardContentDirective
   ],
   imports: [
     BrowserModule
