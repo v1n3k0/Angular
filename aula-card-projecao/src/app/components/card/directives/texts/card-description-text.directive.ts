a8f56e511c930f3972a5b8e7913cf168aa7699c3
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-02-09 09:06:51 -0300

    aula 164

---
+++ aula-card-projecao/src/app/components/card/directives/texts/card-description-text.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCardDescriptionText]',
+  host: {
+    'class': 'ca-c-card__description-text'
+  }
+})
+export class CardDescriptionTextDirective {
+}
