ed56f2c7d15a0ae8a86c17be993ec744ab01d990
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-02-27 07:52:08 -0300

    aula 171

--- aula-ng-template-via-input/src/app/card/card.component.ts
+++ aula-ng-template-via-input/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  @Input({ required: true }) header!: TemplateRef<any>;
+  @Input({ required: true }) body!: TemplateRef<any>;
+  @Input({ required: false }) footer!: TemplateRef<any>;
 }
