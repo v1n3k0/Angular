dd8e4fa233b7f963caec97d1f791ae7fee80d433
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-02-27 07:32:52 -0300

    aula 170

--- aula-problema-com-projecao-conteudo/src/app/app.component.ts
+++ aula-problema-com-projecao-conteudo/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './ng-if/ex1.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  condition: boolean = true;
+}
