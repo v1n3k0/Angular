33c32529a031d0664b7a6d2a460c1c64d67a3093
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-03-13 09:04:44 -0300

    Aula 182

--- aula-pipes/src/app/app.module.ts
+++ aula-pipes/src/app/app.module.ts
@@ -6,6 +6,14 @@ import { UserStatusPipe } from './pipes/user-status.pipes';
 import { TruncatePipe } from './pipes/truncate.pipes';
 import { PipeDataComponent } from './pipe-data/pipe-data.component';
 
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt-BR');
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +24,20 @@ import { PipeDataComponent } from './pipe-data/pipe-data.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { 
+      provide: DATE_PIPE_DEFAULT_OPTIONS, 
+      useValue: 
+      { 
+        dateFormat: 'dd/MM/yyyy HH:mm:ss',
+        timezone: '+0000' 
+      } 
+    },
+    {      
+      provide: LOCALE_ID,
+      useValue: 'pt-BR' 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
