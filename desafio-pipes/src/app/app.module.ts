1ef1b74afc2eb608d20941cca6d6abf96ddad152
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-04-07 09:00:29 -0300

    pipe status

--- desafio-pipes/src/app/app.module.ts
+++ desafio-pipes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StatusPipe } from './pipes/status.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusPipe
   ],
   imports: [
     BrowserModule
