eefdda35f24309c6a8b75afc79d4285d22309364
v1n3k0 <v1n1c1u5_1@hotmail.com>
2026-04-13 09:22:50 -0300

    decimal pipe

--- desafio-pipes/src/app/app.module.ts
+++ desafio-pipes/src/app/app.module.ts
@@ -3,7 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StatusPipe } from './pipes/status.pipe';
-import { StatusImgPipe } from './pipes/status-img.pipes';
+import { StatusImgPipe } from './pipes/status-img.pipe';
+
+import { LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -14,7 +20,16 @@ import { StatusImgPipe } from './pipes/status-img.pipes';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {      
+      provide: LOCALE_ID,
+      useValue: 'pt-BR' 
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
